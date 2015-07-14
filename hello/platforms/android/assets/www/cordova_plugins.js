326d73cade7c3cf49bee70c9a849ab2d62a056f1
Lorenzo Pontellini <lorenzo.pontellini@studio.unibo.it>
2015-07-14 09:26:47 +0200

    no message

--- hello/platforms/android/assets/www/cordova_plugins.js
+++ hello/platforms/android/assets/www/cordova_plugins.js
@@ -1,11 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
-        "id": "cordova-plugin-whitelist.whitelist",
-        "runs": true
-    },
-    {
         "file": "plugins/cordova-plugin-device-motion/www/Acceleration.js",
         "id": "cordova-plugin-device-motion.Acceleration",
         "clobbers": [
@@ -18,13 +13,18 @@ module.exports = [
         "clobbers": [
             "navigator.accelerometer"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0",
-    "cordova-plugin-device-motion": "1.1.1"
+    "cordova-plugin-device-motion": "1.1.1",
+    "cordova-plugin-whitelist": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
