4f7449b3eca85f6bb4b0e889b22bd8d84cc2f2a7
Lorenzo Pontellini <lorenzo.pontellini@studio.unibo.it>
2015-07-15 10:14:32 +0200

    no message

--- www/js/robotWebUI.js
+++ www/js/robotWebUI.js
@@ -68,3 +68,51 @@ function show(who) {
 function hide(who) {
 	document.getElementById(who).style.display = "none";
 }
+
+// The watch id references the current `watchAcceleration`
+    var watchID = null;
+
+    // Wait for device API libraries to load
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        startWatch();
+    }
+
+    // Start watching the acceleration
+    //
+    function startWatch() {
+
+        // Update acceleration every 1/2 seconds
+        var options = { frequency: 500 };
+
+        watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+    }
+
+    // Stop watching the acceleration
+    //
+    function stopWatch() {
+        if (watchID) {
+            navigator.accelerometer.clearWatch(watchID);
+            watchID = null;
+        }
+    }
+
+    // onSuccess: Get a snapshot of the current acceleration
+    //
+    function onSuccess(acceleration) {
+        var element = document.getElementById('accelerometer');
+        element.innerHTML = 'Acceleration X: ' + acceleration.x         + '<br />' +
+                            'Acceleration Y: ' + acceleration.y         + '<br />' +
+                            'Acceleration Z: ' + acceleration.z         + '<br />' +
+                            'Timestamp: '      + acceleration.timestamp + '<br />';
+    }
+
+    // onError: Failed to get the acceleration
+    //
+    function onError() {
+        alert('onError!');
+    }
